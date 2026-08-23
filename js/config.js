7851c72e230f78cb71a9111158041d34d5e06e22
STWNT168 <SATWANT168@GMAIL.COM>
2026-08-24 01:50:08 +0530

    Update API_URL in config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,1 @@
-window.APP_CONFIG={API_URL:'https://script.google.com/macros/s/AKfycbxMYcUS96AkPshSgz2i4bxUiqgLqYg2aKCI58CXVkqoiZrIh582aW-FzOvC6ljhgOkV/exec',TIME_ZONE:'Asia/Kolkata'};
+window.APP_CONFIG={API_URL:'https://script.google.com/macros/s/AKfycbxWXO_1R6i4mKyFhXilAEL6xZ8uZmdqBkFyY3qDzwMEp-XoWi0cTokCzsTK05EUtPhe/exec',TIME_ZONE:'Asia/Kolkata'};
